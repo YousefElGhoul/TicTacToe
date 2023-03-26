c78728d92570365305e1b4c9a8028028b2a60c61
YousefElGhoul <yousefelghoul03@gmail.com>
2023-03-26 17:42:56 +0200

    Misc. refactoring and rearranging of code

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -37,213 +37,20 @@ using namespace std;
 #define VS_STATE_HUM 1
 #define VS_STATE_COM 0
 
-char board[9] = {' ', ' ', ' ', ' ', '^', ' ', ' ', ' ', ' '};
+char board[9] = {};
 char arrow;
 int selector, vs_state = 1, player = 1, counter = 0;
 string player1 = "Player 1", player2 = "Player 2";
 int score1 = 0, score2 = 0;
 
 int isEven(int x){return x % 2 == 0 ? 2 : 1;}
+string getName(int player){return isEven(player) == 1 ? player1 : player2;}
 
-void exitProgram(){
-    exit(0);
-}
-
-class Game{
+class Display{
     public:
         static void initDisplay(){
             system("color a");
             clear();
-        }
-        static void initGame(){
-            clearBoard();
-        }
-        static void menu(int menu){
-            switch (menu){
-            case START:
-                refresh(START_MENU);
-                arrow = getch();
-                do{
-                    switch (arrow){
-                    case KEY_ESC:
-                        exitProgram();
-                    default:
-                        arrow = getch();
-                        switch (arrow){
-                        case KEY_UP:
-                            refresh(START_MENU_START_SELECTED);
-                            selector = 'S';
-                            break;
-                        case KEY_DOWN:
-                            refresh(START_MENU_EXIT_SELECTED);
-                            selector = 'E';
-                            break;
-                        case KEY_ESC:
-                            exitProgram();
-                        default:
-                            break;
-                        }
-                        break;
-                    }
-                }while (arrow != KEY_ENTER);
-                switch (selector){
-                case 'S':
-                    Game::menu(VS);
-                    break;
-                case 'E':
-                    exitProgram();
-                default:
-                    break;
-                }
-                break;
-            case VS:
-                cout << "\nVS Screen\n";
-                Game::menu(NAME);
-                break;
-            case NAME:
-                refresh(LOGO);
-                if(vs_state == VS_STATE_COM){
-                    cout << "Player 1: \n";
-                    getline(cin, player1);
-                    if(player1.empty())
-                        player1 = "Player 1";
-                }
-                else{
-                    cout << "Player 1: \n";
-                    getline(cin, player1);
-                    if(player1.empty())
-                        player1 = "Player 1";
-                    cout << "\nPlayer 2: \n";
-                    getline(cin, player2);
-                    if(player2.empty())
-                        player2 = "Player 2";
-                }
-                Game::menu(GAME);
-                break;
-            case GAME:
-                selector = 0;
-                refresh(LOGO);
-                printBoard();
-                arrow = getch();
-                do{
-                    switch (arrow){
-                    case KEY_ESC:
-                        exitProgram();
-                    default:
-                        arrow = getch();
-                        switch (arrow){
-                        case KEY_UP:
-                            MoveUp();
-                            break;
-                        case KEY_DOWN:
-                            MoveDown();
-                            break;
-                        case KEY_LEFT:
-                            MoveLeft();
-                            break;
-                        case KEY_RIGHT:
-                            MoveRight();
-                            break;
-                        case KEY_ESC:
-                            exitProgram();
-                        default:
-                            break;
-                        }
-                        break;
-                    }
-                }while (arrow != KEY_ENTER);
-                switch (isEven(player)){
-                case 1:
-                    board[selector - 1] = 'X';
-                    break;
-                case 2:
-                    board[selector - 1] = 'O';
-                default:
-                    break;
-                }
-                refresh(LOGO);
-                printBoard();
-                switch (checkWin()){
-                case GAME_STATE_ONGOING:
-                    player++;
-                    Game::menu(GAME);
-                    break;
-                case GAME_STATE_RESULT:
-                    Game::menu(WIN);
-                    break;
-                case GAME_STATE_DRAW:
-                    Game::menu(DRAW);
-                default:
-                    break;
-                }
-                break;
-            case WIN:
-                cout << "\t\t\t\t\t\t" << getName(player) << " Wins!\n";
-                switch (isEven(player)){
-                case 1:
-                    score1++;
-                    break;
-                case 2:
-                    score2++;
-                default:
-                    break;
-                }
-                system("pause");
-                Game::menu(RESTART);
-                break;
-            case DRAW:
-                cout << "\t\t\t\t\t\tGame is a Draw!\n";
-                system("pause");
-                Game::menu(RESTART);
-                break;
-            case RESTART:
-                refresh(RESTART_MENU);
-                arrow = getch();
-                do{
-                    switch (arrow){
-                    case KEY_ESC:
-                        exitProgram();
-                    default:
-                        arrow = getch();
-                        switch (arrow){
-                        case KEY_UP:
-                            refresh(RESTART_MENU_RESTART_SELECTED);
-                            selector = 'R';
-                            break;
-                        case KEY_DOWN:
-                            refresh(RESTART_MENU_EXIT_SELECTED);
-                            selector = 'E';
-                            break;
-                        case KEY_ESC:
-                            exitProgram();
-                        default:
-                            break;
-                        }
-                        break;
-                    }
-                }while (arrow != KEY_ENTER);
-                switch (selector){
-                case 'R':
-                    initGame();
-                    Game::menu(VS);
-                    break;
-                case 'E':
-                    exitProgram();
-                default:
-                    break;
-                }
-                break;
-            default:
-                cout << "\nMissing Screen\n";
-                break;
-            }
-        }
-    private:
-        static void clear(){
-            system("CLS");
-        }
-        static void print(string str){
-            cout << str;
         }
         static void refresh(string str){
             clear();
@@ -262,14 +69,84 @@ class Game{
                  << "\t\t\t\t\t\t  " << board[6] << "  |  " << board[7] << "  |  " << board[8] << endl
                  << "\t\t\t\t\t\t     |     |     " << endl << endl;
         }
+    private:
+        static void clear(){system("CLS");}
+        static void print(string str){cout << str;}
+};
+
+class Navigation{
+    public:
+        static void MoveUp(){
+            if(counter == 3){
+                counter = 0;
+                MoveRight();
+            }
+            selector -= 3;
+            selector = selector <= 0 ? selector + 9 : selector;
+            if (board[selector - 1] == 'X' || board[selector - 1] == 'O'){
+                counter++;
+                MoveUp();
+            }
+            else{
+                Navigation::removeSelector();
+                board[selector - 1] = '^';
+                Display::refresh(LOGO);
+                Display::printBoard();
+            }
+        }
+        static void MoveDown(){
+            if(counter == 3){
+                counter = 0;
+                MoveLeft();
+            }
+            selector += 3;
+            selector = selector > 9 ? selector - 9 : selector;
+            if (board[selector - 1] == 'X' || board[selector - 1] == 'O'){
+                counter++;
+                MoveDown();
+            }
+            else{
+                Navigation::removeSelector();
+                board[selector - 1] = '^';
+                Display::refresh(LOGO);
+                Display::printBoard();
+            }
+        }
+        static void MoveLeft(){
+            selector -= 1;
+            selector = selector <= 0 ? selector + 9 : selector;
+            if (board[selector - 1] == 'X' || board[selector - 1] == 'O')
+                MoveLeft();
+            else{
+                Navigation::removeSelector();
+                board[selector - 1] = '^';
+                Display::refresh(LOGO);
+                Display::printBoard();
+            }
+        }
+        static void MoveRight(){
+            selector += 1;
+            selector = selector > 9 ? selector - 9 : selector;
+            if (board[selector - 1] == 'X' || board[selector - 1] == 'O')
+                MoveRight();
+            else{
+                Navigation::removeSelector();
+                board[selector - 1] = '^';
+                Display::refresh(LOGO);
+                Display::printBoard();
+            }
+        }
         static void removeSelector() {
             for (int i = 0; i < 9; i++)
                 if (board[i] == '^')
                     board[i] = ' ';
         }
-        static void clearBoard() {
-            for (int i = 0; i < 9; i++)
-                board[i] = ' ';
+};
+
+class Game{
+    public:
+        static void initGame(){
+            clearBoard();
         }
         static int checkWin(){
             if (board[0] == board[1] && board[1] == board[2] && board[2] == 'X')
@@ -309,75 +186,203 @@ class Game{
             else
                 return GAME_STATE_ONGOING;
         }
-        static void MoveUp(){
-            if(counter == 3){
-                counter = 0;
-                MoveRight();
-            }
-            selector -= 3;
-            selector = selector <= 0 ? selector + 9 : selector;
-            if (board[selector - 1] == 'X' || board[selector - 1] == 'O'){
-                counter++;
-                MoveUp();
-            }
-            else{
-                removeSelector();
-                board[selector - 1] = '^';
-                refresh(LOGO);
-                printBoard();
-            }
+    private:
+        static void clearBoard() {
+            for (int i = 0; i < 9; i++)
+                board[i] = ' ';
         }
-        static void MoveDown(){
-            if(counter == 3){
-                counter = 0;
-                MoveLeft();
+};
+
+void exitProgram(){exit(0);}
+
+class Menus{
+    public:
+        static void select(int menu){
+            switch (menu){
+            case START:
+                Display::refresh(START_MENU);
+                arrow = getch();
+                do{
+                    switch (arrow){
+                    case KEY_ESC:
+                        exitProgram();
+                    default:
+                        arrow = getch();
+                        switch (arrow){
+                        case KEY_UP:
+                            Display::refresh(START_MENU_START_SELECTED);
+                            selector = 'S';
+                            break;
+                        case KEY_DOWN:
+                            Display::refresh(START_MENU_EXIT_SELECTED);
+                            selector = 'E';
+                            break;
+                        case KEY_ESC:
+                            exitProgram();
+                        default:
+                            break;
+                        }
+                        break;
+                    }
+                }while (arrow != KEY_ENTER);
+                switch (selector){
+                case 'S':
+                    Menus::select(VS);
+                    break;
+                case 'E':
+                    exitProgram();
+                default:
+                    break;
+                }
+                break;
+            case VS:
+                cout << "\nVS Screen\n";
+                Menus::select(NAME);
+                break;
+            case NAME:
+                Display::refresh(LOGO);
+                if(vs_state == VS_STATE_COM){
+                    cout << "Player 1: \n";
+                    getline(cin, player1);
+                    if(player1.empty())
+                        player1 = "Player 1";
+                }
+                else{
+                    cout << "Player 1: \n";
+                    getline(cin, player1);
+                    if(player1.empty())
+                        player1 = "Player 1";
+                    cout << "\nPlayer 2: \n";
+                    getline(cin, player2);
+                    if(player2.empty())
+                        player2 = "Player 2";
+                }
+                Menus::select(GAME);
+                break;
+            case GAME:
+                selector = 0;
+                Display::refresh(LOGO);
+                Display::printBoard();
+                arrow = getch();
+                do{
+                    switch (arrow){
+                    case KEY_ESC:
+                        exitProgram();
+                    default:
+                        arrow = getch();
+                        switch (arrow){
+                        case KEY_UP:
+                            Navigation::MoveUp();
+                            break;
+                        case KEY_DOWN:
+                            Navigation::MoveDown();
+                            break;
+                        case KEY_LEFT:
+                            Navigation::MoveLeft();
+                            break;
+                        case KEY_RIGHT:
+                            Navigation::MoveRight();
+                            break;
+                        case KEY_ESC:
+                            exitProgram();
+                        default:
+                            break;
+                        }
+                        break;
+                    }
+                }while (arrow != KEY_ENTER);
+                switch (isEven(player)){
+                case 1:
+                    board[selector - 1] = 'X';
+                    break;
+                case 2:
+                    board[selector - 1] = 'O';
+                default:
+                    break;
+                }
+                Display::refresh(LOGO);
+                Display::printBoard();
+                switch (Game::checkWin()){
+                case GAME_STATE_ONGOING:
+                    player++;
+                    Menus::select(GAME);
+                    break;
+                case GAME_STATE_RESULT:
+                    Menus::select(WIN);
+                    break;
+                case GAME_STATE_DRAW:
+                    Menus::select(DRAW);
+                default:
+                    break;
+                }
+                break;
+            case WIN:
+                cout << "\t\t\t\t\t\t" << getName(player) << " Wins!\n";
+                switch (isEven(player)){
+                case 1:
+                    score1++;
+                    break;
+                case 2:
+                    score2++;
+                default:
+                    break;
+                }
+                system("pause");
+                Menus::select(RESTART);
+                break;
+            case DRAW:
+                cout << "\t\t\t\t\t\tGame is a Draw!\n";
+                system("pause");
+                Menus::select(RESTART);
+                break;
+            case RESTART:
+                Display::refresh(RESTART_MENU);
+                arrow = getch();
+                do{
+                    switch (arrow){
+                    case KEY_ESC:
+                        exitProgram();
+                    default:
+                        arrow = getch();
+                        switch (arrow){
+                        case KEY_UP:
+                            Display::refresh(RESTART_MENU_RESTART_SELECTED);
+                            selector = 'R';
+                            break;
+                        case KEY_DOWN:
+                            Display::refresh(RESTART_MENU_EXIT_SELECTED);
+                            selector = 'E';
+                            break;
+                        case KEY_ESC:
+                            exitProgram();
+                        default:
+                            break;
+                        }
+                        break;
+                    }
+                }while (arrow != KEY_ENTER);
+                switch (selector){
+                case 'R':
+                    Game::initGame();
+                    Menus::select(VS);
+                    break;
+                case 'E':
+                    exitProgram();
+                default:
+                    break;
+                }
+                break;
+            default:
+                cout << "\nMissing Screen\n";
+                break;
             }
-            selector += 3;
-            selector = selector > 9 ? selector - 9 : selector;
-            if (board[selector - 1] == 'X' || board[selector - 1] == 'O'){
-                counter++;
-                MoveDown();
-            }
-            else{
-                removeSelector();
-                board[selector - 1] = '^';
-                refresh(LOGO);
-                printBoard();
-            }
-        }
-        static void MoveLeft(){
-            selector -= 1;
-            selector = selector <= 0 ? selector + 9 : selector;
-            if (board[selector - 1] == 'X' || board[selector - 1] == 'O')
-                MoveLeft();
-            else{
-                removeSelector();
-                board[selector - 1] = '^';
-                refresh(LOGO);
-                printBoard();
-            }
-        }
-        static void MoveRight(){
-            selector += 1;
-            selector = selector > 9 ? selector - 9 : selector;
-            if (board[selector - 1] == 'X' || board[selector - 1] == 'O')
-                MoveRight();
-            else{
-                removeSelector();
-                board[selector - 1] = '^';
-                refresh(LOGO);
-                printBoard();
-            }
-        }
-        static string getName(int player){
-            return isEven(player) == 1 ? player1 : player2;
         }
 };
 
 void initProgram(){
-    Game::initDisplay();
+    Display::initDisplay();
     Game::initGame();
-    Game::menu(START);
+    Menus::select(START);
 }
 
 int main(){
